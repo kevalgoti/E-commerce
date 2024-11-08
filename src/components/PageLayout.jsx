5fbef0fd2196a632a54a6ef7ddafcb3eb64c66b6
Harshil Goti <harshil.goti@sprinklr.com>
2024-11-08 18:26:30 +0530

    design fix

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,53 +1,46 @@
-'use client';
-import { useState, useEffect } from "react";
+"use client";
+import { useState, useEffect, useCallback } from "react";
 import Header from "../components/header/Header";
 import Sidebar from "../components/sideNavbar/SideNavbar";
 
 function PageLayout({ children }) {
   const [isOpen, setIsOpen] = useState(false); // Initially closed on small screens
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
 
-  useEffect(() => {
-    const checkScreenSize = () => {
-      const isNowSmallScreen = window.innerWidth <= 768;
-      setIsSmallScreen(isNowSmallScreen);
+  // const screenSize = window?.innerWidth;
+  // const isSmallScreen = screenSize <= 768;
 
-      if (isNowSmallScreen) {
-        setIsOpen(false); // Close sidebar on smaller screens
-      } else {
-        setIsOpen(true); // Open sidebar on larger screens
-      }
-    };
+  // useEffect(() => {
+  //   const checkScreenSize = () => {
+  //     const isNowSmallScreen = window.innerWidth <= 768;
+  //     setIsSmallScreen(isNowSmallScreen);
 
-    window.addEventListener("resize", checkScreenSize);
-    checkScreenSize(); // Initial check on mount
-    return () => window.removeEventListener("resize", checkScreenSize);
-  }, []);
+  //     if (isNowSmallScreen) {
+  //       setIsOpen(false);
+  //     } else {
+  //       setIsOpen(true);
+  //     }
+  //   };
 
-  const toggleSidebar = () => setIsOpen((prev) => !prev);
+  //   window.addEventListener("resize", checkScreenSize);
+  //   checkScreenSize(); // Initial check on mount
+  //   return () => window.removeEventListener("resize", checkScreenSize);
+  // }, []);
+
+  const toggleSidebar = useCallback(() => setIsOpen((prev) => !prev), []);
 
   return (
     <div className="min-h-screen flex bg-gray-100 overflow-hidden">
-      {/* Sidebar */}
       <div
         className={`${
           isOpen ? "block" : "hidden"
-        } md:block fixed md:relative z-20 w-64 bg-white h-full transition-transform duration-300`}
+        } md:block fixed md:relative z-20 bg-white h-full transition-transform duration-300`}
       >
         <Sidebar />
       </div>
-
-      {/* Main content */}
-      <div
-        className={`flex flex-col w-full h-full transition-all duration-300 ${
-          isOpen && !isSmallScreen ? "md:ml-64" : ""
-        }`}
-      >
+      <div className="flex flex-col w-full h-full overflow-x-auto transition-all duration-300">
         <Header toggleSidebar={toggleSidebar} />
         <main className="flex-1 overflow-y-auto p-5">
-          <div className="w-full h-full overflow-hidden">
-            {children}
-          </div>
+          <div className="w-full h-full overflow-hidden">{children}</div>
         </main>
       </div>
     </div>
